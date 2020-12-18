a65a24fe722e4704d960ece848cb48c7f297798b
Garrett <gman9mm@live.com>
2020-12-18 11:17:48 -0800

    Add Vendor Constructor Test.

---
+++ PierresVendors.Tests/ModelTests/VendorTest.cs
@@ -0,0 +1,23 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PierresVendors.Models;
+using System.Collections.Generic;
+using System;
+
+namespace PierresVendors.Tests
+{
+  [TestClass]
+  public class VendorTest : IDisposable
+  {
+    public void Dispose()
+    {
+      Vendor.ClearAll();
+    }
+    [TestMethod]
+    public void VendorConstructor_ReturnsInstance_Vendor()
+    {
+      Vendor newVendor = new Vendor("test");
+      Assert.AreEqual(typeof(Vendor), newerVendor.GetType());
+    }
+  }
+
+}
