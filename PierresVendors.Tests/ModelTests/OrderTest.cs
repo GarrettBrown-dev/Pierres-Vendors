cfb766ab948d28c29d483b4ac57b5c37bf67f358
Garrett <gman9mm@live.com>
2020-12-18 10:21:03 -0800

    Successful Order Constructor Test.

--- PierresVendors.Tests/ModelTests/OrderTest.cs
+++ PierresVendors.Tests/ModelTests/OrderTest.cs
@@ -17,8 +17,28 @@ namespace PierresVendors.Tests
         public void OrderConstructor_CreatesInstanceOfOrder_Order()
         {
             Order newOrder = new Order("test", "test", "test", "test");
-            Assert.AreEqual(typeof(Order), Order.GetType());
+            Assert.AreEqual(typeof(Order), newOrder.GetType());
         }
+        // [TestMethod]
+        // public void GetName_ReturnsName_String()
+        // {
+        // string name = "P-Diddy";
+        // string date = "03/15/2019";
+        // string desc = "Rappers Are Cool Event.";
+        // string cost = "$50,000";
+
+        // Order newOrder = new Order(name, date, desc, cost);
+        // string result1 = newOrder.Name;
+        // string result2 = newOrder.Date;
+        // string result3 = newOrder.Desc;
+        // string result4 = newOrder.Cost;
+
+        // Assert.AreEqual(name, result1);
+        // Assert.AreEqual(date, result2);
+        // Assert.AreEqual(desc, result3);
+        // Assert.AreEqual(cost, result4);
+        // }
 
     }
+
 }
