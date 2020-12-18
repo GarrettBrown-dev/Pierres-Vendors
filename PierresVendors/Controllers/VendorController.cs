adef01997c959b96b86b6c422d617fdc629d8944
Garrett <gman9mm@live.com>
2020-12-18 15:48:10 -0800

    Add Vendor Delete Function.

--- PierresVendors/Controllers/VendorController.cs
+++ PierresVendors/Controllers/VendorController.cs
@@ -49,5 +49,12 @@ namespace PierresVendors.Controllers
       model.Add("vendor", foundVendor);
       return View("Show", model);
     }
+
+    [HttpPost("/vendors/delete")]
+    public ActionResult Delete()
+    {
+      Vendor.ClearAll();
+      return View();
+    }
   }
 }
